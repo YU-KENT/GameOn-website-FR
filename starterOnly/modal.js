82793bb0fb2550960dcfa4f0c12f9dd3b011be9d
YU KENT <tianyu.tina@gmail.com>
2022-12-17 21:26:21 +0100

    validation formumaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,7 +1,7 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  const x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += "responsive";
+    x.classList.add ("responsive"); //correct add classname
   } else {
     x.className = "topnav";
   }
@@ -11,6 +11,11 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const inscription = document.querySelector(".bground > .content");
+const birth = document.getElementById("birthdate");
+const prenom = document.getElementById("first");
+const nom = document.getElementById("last");
+const email = document.getElementById("email");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +23,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
  modalbg.style.display = "block";
+ newDiv.style.display = "none";
 };
 
 
@@ -32,9 +38,9 @@ nav.removeChild(navAccueil);
 //add div validation message
 const newDiv = document.createElement("div");
 newDiv.className = "validation";
-modalbg.appendChild(newDiv);
 newDiv.innerHTML = "<p>Merci pour</p><p>Votre Inscription</p>";
 
+modalbg.appendChild(newDiv);
 
 const arrowClose = document.createElement("span");
 arrowClose.classList.add("close");
@@ -45,10 +51,6 @@ btnClose.className = "btn-close";
 btnClose.innerHTML = "Fermer";
 newDiv.appendChild(btnClose);
 
-// add event button submit "c'est partir"
-const inscription = document.querySelector(".bground > .content");
-
-
 
 
 //validate form ,The data must be entered correctly
@@ -56,66 +58,202 @@ const inscription = document.querySelector(".bground > .content");
 document.getElementById("myform")
 .addEventListener("submit",validate);
 
-  function validate(e) {
-  var prenom = document.getElementById("first");
-  var nom = document.getElementById("last");
-  var email = document.getElementById("email");
-  var birth = document.getElementById("birthdate");
+  function validate(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const conditions = 
+    [validatePrenom(),validateNom(),validateEmail(),validateBirth(),
+    validateQuantity(),validateRadio(),validateCondition()]
+  if (conditions.filter((func) => !func))
+      {
+        return;
+      }
+      showMessage();
+      console.log("KKK");
 
-  if (prenom.value.length < 2) {
-    prenom.setCustomValidity("Le champ Prénom a un minimum de 2 caractères");
-    e.preventDefault();
-    return false;
-  
-  }else if (prenom.value.trim() = "" ) {
-    prenom.setCustomValidity("n\'est pas vide");
-    e.preventDefault();
-    return false;
+};
+//validate prenom
 
-  }else if (nom.value.length < 2) {
-    nom.setCustomValidity("Le champ Prénom a un minimum de 2 caractères");
-    e.preventDefault();
-    return false;
+function validatePrenom(){
+if (prenom.value.trim() === "" || prenom.value.length < 2) /* not empty or at least 2 characters */
+{
+formData[0].setAttribute("data-error","Veuillez entrer 2 caractères ou plus pour le champ du prenom");
+formData[0].setAttribute("data-error-visible", true);
+return false;
+}
+else{
+formData[0].removeAttribute('data-error');
+formData[0].removeAttribute('data-error-visible');
+return true;
+}
 
-  } else if (nom.value.trim() = "") {
-    nom.setCustomValidity("n\'est pas vide");
-    e.preventDefault();
-    return false;
-  } else if (email.value.trim()= ""){
-    email.setCustomValidity("L\'adresse électronique est valide.");
-    e.preventDefault();
-
-  } else if (/^[0-9]{8}$/.test(birth.value) = false){
-    birth.setCustomValidity("Pour le nombre de concours, une valeur numérique est saisie.")
-    e.preventDefault();
-  }
-  
- 
-   else{return true;}
-  };
-  
-
-
- // function show validation message
- //if(validate() === true){
- //function showMessage() {
- //inscription.style.display = "none";
- //newDiv.style.display = "flex"; 
- //}};
-
-
-
-//add "close" function
-
-
-document.querySelector(".content > .close")
-.addEventListener("click",closeWindow);
-arrowClose.addEventListener("click",closeWindow);
-
-function closeWindow() {
-modalbg.style.display = "none"
 };
 
+// validate nom
+function validateNom(){
+if (nom.value.trim() === "" || nom.value.length < 2) /* not empty or at least 2 characters */
+    {
+    formData[1].setAttribute('data-error','Veuillez entrer 2 caractères ou plus pour le champ du nom');
+    formData[1].setAttribute('data-error-visible',true);
+    return false;
+    
+    }
+else{
+      formData[1].removeAttribute('data-error');
+      formData[1].removeAttribute('data-error-visible');
+      return true;
+      }
+  };
+
+
+// validate email
+function validateEmail(){
+const regexEmail =
+/^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/; /*regex mail*/
+
+if (!regexEmail.test(email.value) || email.value.trim() === "" )/* not empty or match regex condition */
+{
+formData[2].setAttribute('data-error','Veuillez inclure \"@\" dans l\'adress e-mail');
+formData[2].setAttribute('data-error-visible',true);
+console.log("email wrong");
+return false;
+}
+
+else{
+  formData[2].removeAttribute('data-error');
+  formData[2].removeAttribute('data-error-visible');
+  return true;
+  }
+}
+// validate birthday
+
+function validateBirth(){
+
+if (birth.value == "") 
+{
+  formData[3].setAttribute('data-error','Vous devez entrer votre date de naissance');
+  formData[3].setAttribute('data-error-visible',true);
+  return false;
+}
+ else {
+  formData[3].removeAttribute('data-error');
+  formData[3].removeAttribute('data-error-visible');
+  return true;
+ }
+}
+
+
+//function birthday can't be current day
+function requireBirth () {
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth();
+     
+if ( month < 10) // if number of month below 10, add "0" before
+    {newMonth = "0" + (month + 1)} 
+else { newMonth = month + 1};
+  
+if (today.getDate() < 10) {  //if number of day below 10, add "0" before
+  newDay = "0"+ today.getDate()
+}
+else {
+  newDay = today.getDate()
+};
+
+birth.setAttribute("max",year+"-"+newMonth+"-"+newDay);
+} ;
+
+requireBirth (); //run function
+
+// function vilidate quantity
+const quantitySelect = document.querySelector("input[name='quantity']");
+
+function validateQuantity(){
+if(quantitySelect.value == "")
+{
+  formData[4].setAttribute('data-error','Veuillez choisir un chiffre');
+  formData[4].setAttribute('data-error-visible',true);
+  return false;
+}
+ else {
+  formData[4].removeAttribute('data-error');
+  formData[4].removeAttribute('data-error-visible');
+  return true;
+ };
+
+};
+// vilidate the city
+
+ //get all input in one Array
+
+function validateRadio(){
+
+  const checkRadio = document.querySelector("input[name='location']:checked");
+  if(!checkRadio){ // if radio is not chosen, show error message
+    formData[5].setAttribute('data-error','Veuillez choisir une ville');
+    formData[5].setAttribute('data-error-visible',true);
+    return false;}
+
+  else {
+      formData[5].removeAttribute('data-error');
+      formData[5].removeAttribute('data-error-visible');
+    return true;
+   }};
+
+
+// validate general condition
+
+  const checkBox1 = document.getElementById("checkbox1");
+  const checkBox2 = document.getElementById("checkbox2");
+
+  function validateCondition(){
+    if(!checkBox1.checked == true){  //if first condition is not chosen
+    formData[6].setAttribute('data-error','Veuillez vérifier que vous acceptez les conditions');
+    formData[6].setAttribute('data-error-visible',true);
+    return false;
+    }
+    
+    else {
+      formData[6].removeAttribute('data-error');
+      formData[6].removeAttribute('data-error-visible');
+    return true;
+        }
+  };
+    
+
+ // function show validation message
+
+ function showMessage() {
+
+  inscription.style.display = "none";
+  newDiv.style.display = "flex";
+ };
+ 
 // add event close validation message
 
 btnClose.addEventListener("click",closeWindow);
+
+
+// fucntion close window by click the arrow
+document.querySelectorAll(".close")
+.forEach(close => close.addEventListener("click",closeWindow));
+
+function closeWindow() {
+modalbg.style.display = "none"; 
+location.reload();/*reload page*/
+};
+
+// add logo and nav into modal, version mobile
+
+const copy = document.getElementById("myTopnav").cloneNode(true);
+copy.setAttribute('id','myTopnav1');
+modalbg.appendChild(copy);
+modalbg.insertBefore(copy,inscription); //in html, deplace div topnav before modal 
+//test code
+const regexBirthday = /^([0-9]{2})([0-9]{2})([0-9]{4})$/;
+
+function aaa(){
+console.log(copy);
+/* console.log(checkBox1.checked == true);*/
+};
+aaa(); 
